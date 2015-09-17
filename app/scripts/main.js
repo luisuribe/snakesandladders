dae67f750e7a3d64b2507ef56be799f0eeaa3ad0
Luis Uribe <acme@eviled.org>
2015-09-16 22:57:23 -0500

    Small changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,93 +4,93 @@ http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/plugins/CSSPlugin.min.js
 */
 
 var grid = {
-    "1": { x: '30px', y: '-10px'},
-    "2": { x: '90px', y: '-10px'},
-    "3": { x: '150px', y: '-10px'},
-    "4": { x: '210px', y: '-10px'},
-    "5": { x: '270px', y: '-10px'},
-    "6": { x: '330px', y: '-10px'},
-    "7": { x: '390px', y: '-10px'},
-    "8": { x: '450px', y: '-10px'},
-    "9": { x: '510px', y: '-10px'},
-    "10": { x: '570px', y: '-10px'},
+    "1": { x: '30', y: '-10'},
+    "2": { x: '90', y: '-10'},
+    "3": { x: '150', y: '-10'},
+    "4": { x: '210', y: '-10'},
+    "5": { x: '270', y: '-10'},
+    "6": { x: '330', y: '-10'},
+    "7": { x: '390', y: '-10'},
+    "8": { x: '450', y: '-10'},
+    "9": { x: '510', y: '-10'},
+    "10": { x: '570', y: '-10'},
 
-    "20": { x: '30px', y: '-70px'},
-    "19": { x: '90px', y: '-70px'},
-    "18": { x: '150px', y: '-70px'},
-    "17": { x: '210px', y: '-70px'},
-    "16": { x: '270px', y: '-70px'},
-    "15": { x: '330px', y: '-70px'},
-    "14": { x: '390px', y: '-70px'},
-    "13": { x: '450px', y: '-70px'},
-    "12": { x: '510px', y: '-70px'},
-    "11": { x: '570px', y: '-70px'},
+    "20": { x: '30', y: '-70'},
+    "19": { x: '90', y: '-70'},
+    "18": { x: '150', y: '-70'},
+    "17": { x: '210', y: '-70'},
+    "16": { x: '270', y: '-70'},
+    "15": { x: '330', y: '-70'},
+    "14": { x: '390', y: '-70'},
+    "13": { x: '450', y: '-70'},
+    "12": { x: '510', y: '-70'},
+    "11": { x: '570', y: '-70'},
 
-    "21": { x: '30px', y: '-130px'},
-    "22": { x: '90px', y: '-130px'},
-    "23": { x: '150px', y: '-130px'},
-    "24": { x: '210px', y: '-130px'},
-    "25": { x: '270px', y: '-130px'},
-    "26": { x: '330px', y: '-130px'},
-    "27": { x: '390px', y: '-130px'},
-    "28": { x: '450px', y: '-130px'},
-    "29": { x: '510px', y: '-130px'},
-    "30": { x: '570px', y: '-130px'},
+    "21": { x: '30', y: '-130'},
+    "22": { x: '90', y: '-130'},
+    "23": { x: '150', y: '-130'},
+    "24": { x: '210', y: '-130'},
+    "25": { x: '270', y: '-130'},
+    "26": { x: '330', y: '-130'},
+    "27": { x: '390', y: '-130'},
+    "28": { x: '450', y: '-130'},
+    "29": { x: '510', y: '-130'},
+    "30": { x: '570', y: '-130'},
 
-    "40": { x: '30px', y: '-190px'},
-    "39": { x: '90px', y: '-190px'},
-    "38": { x: '150px', y: '-190px'},
-    "37": { x: '210px', y: '-190px'},
-    "36": { x: '270px', y: '-190px'},
-    "35": { x: '330px', y: '-190px'},
-    "34": { x: '390px', y: '-190px'},
-    "33": { x: '450px', y: '-190px'},
-    "32": { x: '510px', y: '-190px'},
-    "31": { x: '570px', y: '-190px'},
+    "40": { x: '30', y: '-190'},
+    "39": { x: '90', y: '-190'},
+    "38": { x: '150', y: '-190'},
+    "37": { x: '210', y: '-190'},
+    "36": { x: '270', y: '-190'},
+    "35": { x: '330', y: '-190'},
+    "34": { x: '390', y: '-190'},
+    "33": { x: '450', y: '-190'},
+    "32": { x: '510', y: '-190'},
+    "31": { x: '570', y: '-190'},
 
-    "41": { x: '30px', y: '-250px'},
-    "42": { x: '90px', y: '-250px'},
-    "43": { x: '150px', y: '-250px'},
-    "44": { x: '210px', y: '-250px'},
-    "45": { x: '270px', y: '-250px'},
-    "46": { x: '330px', y: '-250px'},
-    "47": { x: '390px', y: '-250px'},
-    "48": { x: '450px', y: '-250px'},
-    "49": { x: '510px', y: '-250px'},
-    "50": { x: '570px', y: '-250px'},
+    "41": { x: '30', y: '-250'},
+    "42": { x: '90', y: '-250'},
+    "43": { x: '150', y: '-250'},
+    "44": { x: '210', y: '-250'},
+    "45": { x: '270', y: '-250'},
+    "46": { x: '330', y: '-250'},
+    "47": { x: '390', y: '-250'},
+    "48": { x: '450', y: '-250'},
+    "49": { x: '510', y: '-250'},
+    "50": { x: '570', y: '-250'},
 
-    "60": { x: '30px', y: '-310px'},
-    "59": { x: '90px', y: '-310px'},
-    "58": { x: '150px', y: '-310px'},
-    "57": { x: '210px', y: '-310px'},
-    "56": { x: '270px', y: '-310px'},
-    "55": { x: '330px', y: '-310px'},
-    "54": { x: '390px', y: '-310px'},
-    "53": { x: '450px', y: '-310px'},
-    "52": { x: '510px', y: '-310px'},
-    "51": { x: '570px', y: '-310px'},
+    "60": { x: '30', y: '-310'},
+    "59": { x: '90', y: '-310'},
+    "58": { x: '150', y: '-310'},
+    "57": { x: '210', y: '-310'},
+    "56": { x: '270', y: '-310'},
+    "55": { x: '330', y: '-310'},
+    "54": { x: '390', y: '-310'},
+    "53": { x: '450', y: '-310'},
+    "52": { x: '510', y: '-310'},
+    "51": { x: '570', y: '-310'},
 
-    "61": { x: '30px', y: '-370px'},
-    "62": { x: '90px', y: '-370px'},
-    "63": { x: '150px', y: '-370px'},
-    "64": { x: '210px', y: '-370px'},
-    "65": { x: '270px', y: '-370px'},
-    "66": { x: '330px', y: '-370px'},
-    "67": { x: '390px', y: '-370px'},
-    "68": { x: '450px', y: '-370px'},
-    "69": { x: '510px', y: '-370px'},
-    "70": { x: '570px', y: '-370px'},
+    "61": { x: '30', y: '-370'},
+    "62": { x: '90', y: '-370'},
+    "63": { x: '150', y: '-370'},
+    "64": { x: '210', y: '-370'},
+    "65": { x: '270', y: '-370'},
+    "66": { x: '330', y: '-370'},
+    "67": { x: '390', y: '-370'},
+    "68": { x: '450', y: '-370'},
+    "69": { x: '510', y: '-370'},
+    "70": { x: '570', y: '-370'},
 
-    "80": { x: '30px', y: '-430px'},
-    "79": { x: '90px', y: '-430px'},
-    "78": { x: '150px', y: '-430px'},
-    "77": { x: '210px', y: '-430px'},
-    "76": { x: '270px', y: '-430px'},
-    "75": { x: '330px', y: '-430px'},
-    "74": { x: '390px', y: '-430px'},
-    "73": { x: '450px', y: '-430px'},
-    "72": { x: '510px', y: '-430px'},
-    "71": { x: '570px', y: '-430px'},
+    "80": { x: '30', y: '-430'},
+    "79": { x: '90', y: '-430'},
+    "78": { x: '150', y: '-430'},
+    "77": { x: '210', y: '-430'},
+    "76": { x: '270', y: '-430'},
+    "75": { x: '330', y: '-430'},
+    "74": { x: '390', y: '-430'},
+    "73": { x: '450', y: '-430'},
+    "72": { x: '510', y: '-430'},
+    "71": { x: '570', y: '-430'},
 
 };
 
@@ -98,16 +98,84 @@ window.onload = function() {
     var box1          = document.getElementById("box1");
     var box1_position = 0;
 
+    var box2          = document.getElementById("box2");
+    var box2_position = 0;
+
     $("#dice1").click(function(){
-        var to = Math.floor((Math.random() * 6) + 1);
-        console.log(to);
+        var to        = Math.floor((Math.random() * 6) + 1);
+        var old_position  = box1_position;
         box1_position = box1_position + to;
-        console.log(box1_position);
-        moveTween(box1_position);
+        moveTween(box1_position, old_position, box1);
     });
 
-    var moveTween = function(to){
-        TweenLite.to(box1, 0.7, grid[to]);
+    $("#dice2").click(function(){
+        var to = Math.floor((Math.random() * 6) + 1);
+        var old_position  = box2_position;
+        box2_position = box2_position + to;
+        moveTween(box2_position, old_position, box2);
+    });
+
+    var moveTween = function(to, old_position, box){
+      var new_position = {x : grid[to].x, y : grid[to].y};
+
+      // if (box == "box2") {
+      //   // new_position.x = new_position.x +10;
+      // }
+
+        new_position.x = new_position.x + 'px';
+        new_position.y = new_position.y + 'px';
+
+        TweenLite.to(box, 0.7, new_position);  
+
+      // if (old_position == 0) {
+
+      //     new_position.x = new_position.x + 'px';
+      //     new_position.y = new_position.y + 'px';
+
+      //     TweenLite.to(box, 0.7, new_position);  
+
+      //   } else {
+
+      //     console.log(old_position);
+      //     console.log(to);
+      //     console.log(Math.ceil ( to / 10 ) + " " + Math.ceil ( old_position / 10 ));
+
+      //     if (Math.ceil ( to / 10 ) != Math.ceil ( old_position / 10 )) {
+      //       var corner             = Math.ceil ( to / 10 );
+      //       var corner_position    = {x : grid[corner].x, y : grid[corner].y}
+      //       var corner_up_position = {x : grid[corner + 1].x, y : grid[corner + 1].y}
+
+      //       corner_position.x = corner_position.x + 'px';
+      //       corner_position.y = corner_position.y + 'px';
+
+      //       corner_up_position.x = corner_up_position.x + 'px';
+      //       corner_up_position.y = corner_up_position.y + 'px';
+
+      //       TweenLite.to(box, 0.7, corner_position);  
+      //       TweenLite.to(box, 0.7, corner_up_position);  
+
+      //       console.log(corner_position);
+      //       console.log(corner_up_position);
+      //       console.log(new_position);
+
+      //       // first, we need to go to the end
+      //       // then to the next
+      //       // then to the 
+      //       new_position.x = new_position.x + 'px';
+      //       new_position.y = new_position.y + 'px';
+
+      //       // TweenLite.to(box, 0.7, new_position);  
+
+
+      //     } else {
+
+      //       new_position.x = new_position.x + 'px';
+      //       new_position.y = new_position.y + 'px';
+
+      //       TweenLite.to(box, 0.7, new_position);  
+      //     }
+      //   }
+
     }
 
 
